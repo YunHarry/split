05c96d3bee5f8fb544d9f5133b46ba379479d45c
harry <harry.backend@gmail.com>
2023-10-04 10:51:46 +0700

    registration form and btn updated

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def hello_world(request):
-    return render(request, "accountapp/hello_world.html")
+    #POST request
+    if request.method == "POST":
+        temp = request.POST.get('email')
+        return render(request, "accountapp/hello_world.html",
+                      context={"temp": temp})
+
+    #GET request
+    temp = "split"
+    return render(request, "accountapp/hello_world.html",
+                  context={"temp": temp})
