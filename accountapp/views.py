b7bee1603be862cea24f6b896abab66ccb14a57c
harry <harry.backend@gmail.com>
2023-10-04 11:01:42 +0700

    reg_all updated

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import Registration
+
+
 # Create your views here.
+
 def hello_world(request):
     #POST request
     if request.method == "POST":
         temp = request.POST.get('email')
+        reg_all = Registration.objects.all()
         return render(request, "accountapp/hello_world.html",
-                      context={"temp": temp})
+                      context={"temp": temp,
+                               "reg_all": reg_all})
 
     #GET request
     temp = "split"
