41179d402ef33bde87b88313b017b1d46bfa49ba
harry <harry.backend@gmail.com>
2023-10-13 11:45:34 +0700

    profile update decorator updated

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileForm
 from profileapp.models import Profile
 
@@ -19,3 +21,15 @@ class ProfileCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    form_class = ProfileForm
+    context_object_name = 'target_profile'
+    template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail',
+                       kwargs={'pk':self.request.user.pk})
